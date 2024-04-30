10ee868d559709094a3b7e9a45674a3788e22f93
cketuveufrr <147418448+cketuveufrr@users.noreply.github.com>
2024-04-30 22:15:23 +0200

    again config problem
    ptdr

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_802_11ay-Raytracing-Simulator_FILE D:/Notes/Couur/Cours_23_24/elech304/projet/802_11ay-Raytracing-Simulator/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/802_11ay-Raytracing-Simulator.exe)
+set(__QT_DEPLOY_TARGET_802_11ay-Raytracing-Simulator_RUNTIME_DLLS D:/Windows/Programs/QT/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Windows/Programs/QT/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Windows/Programs/QT/6.8.0/mingw_64/bin/Qt6Core.dll)
