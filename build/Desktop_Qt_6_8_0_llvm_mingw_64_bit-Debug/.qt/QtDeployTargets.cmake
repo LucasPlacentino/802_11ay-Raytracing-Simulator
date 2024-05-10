7210a11342ebaaa2b2619eddfb95cb33d6a99c19
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-05-10 16:19:46 +0200

    Delete build directory

--- build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_802_11ay-Raytracing-Simulator_FILE C:/Users/Salman/Desktop/802_11ay-Raytracing-Simulator-1/build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/802_11ay-Raytracing-Simulator.exe)
-set(__QT_DEPLOY_TARGET_802_11ay-Raytracing-Simulator_RUNTIME_DLLS D:/Windows/Programs/QT/6.8.0/llvm-mingw_64/bin/Qt6Widgets.dll;D:/Windows/Programs/QT/6.8.0/llvm-mingw_64/bin/Qt6Gui.dll;D:/Windows/Programs/QT/6.8.0/llvm-mingw_64/bin/Qt6Core.dll)
--- build/unknown_unknown_unknown_elf_16bit-Debug3/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/Salman/Desktop/oriented/802_11ay-Raytracing-Simulator/build/unknown_unknown_unknown_elf_16bit-Debug3/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "D:/BTN/Microchip/xc16/v2.10/bin/xc16-gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "" CACHE "FILEPATH" "" FORCE)
