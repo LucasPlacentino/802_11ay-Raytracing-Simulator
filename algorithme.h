984bed73671e0f2286e226288a82ee7b3a3eae93
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-05-17 22:03:43 +0200

    Update algorithme.h

--- algorithme.h
+++ algorithme.h
@@ -3,20 +3,6 @@
 
 #include "simulation.h"
 
-class Algorithme {
-public:
-    /*
-    Algorithme(Simulation* simulation, qreal resolution);
-    void runBruteForceOptimization();
-    void setTransmitterPosition(Transmitter* transmitter, qreal x, qreal y);
-    qreal computeTotalPowerAtPosition(Transmitter* transmitter, qreal x, qreal y);
-    */
-
-private:
-    Simulation* simulation;
-    qreal resolution;
-};
-
 void runAlgo();
 
 #endif // ALGORITHME_H
