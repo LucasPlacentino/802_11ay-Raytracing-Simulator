94fc9899a93a87592437f5f4434801a969829002
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-05-17 22:06:25 +0200

    Update algorithme.cpp

--- algorithme.cpp
+++ algorithme.cpp
@@ -83,65 +83,11 @@ void runAlgo() {
     }
     qDebug() << "for loop finished";
 
-
-    /*
-    Simulation* bestSimAveragePower = nullptr;
-    Simulation* bestSimLessDisconnectedCells = nullptr;
-    qreal best_average_pow_dbm = 0;
-    int min_num_of_disconnected_cells = sim_list[0]->cells.length()*sim_list[0]->cells[0].length();
-    int g=0;
-    while (sim_list.length() != 0) {
-    //}
-    //for (Simulation* sim : sim_list) {
-        Simulation* sim = sim_list[0];
-        //sim->run(nullptr); // run sim
-
-        bool better = false;
-        g++;
-        qreal total_pow_dbm = 0;
-        int num_of_disconnected_cells = 0;
-        int i = 0;
-        for (QList<Receiver*> cell_line : sim->cells) {
-            for (Receiver* cell : cell_line) {
-                i++;
-
-                qreal _rx_power = cell->computeTotalPower(sim->baseStations[0]);
-                cell->power = _rx_power;
-                cell->updateBitrateAndColor();
-
-                total_pow_dbm += 10*log10(cell->power*1000);
-                if (10*std::log10(cell->power*1000) < -90) {
-                    num_of_disconnected_cells++;
-                }
-                //qDebug() << "Cell" << i;
-            }
-        }
-        qreal average_pow_dbm = total_pow_dbm/((sim->cells.length()) * (sim->cells[0].length()));
-        if (average_pow_dbm > best_average_pow_dbm) {
-            // this sim is a better sim
-            best_average_pow_dbm = average_pow_dbm;
-            bestSimAveragePower = sim;
-            better = true;
-        }
-        if (num_of_disconnected_cells < min_num_of_disconnected_cells) {
-            min_num_of_disconnected_cells = num_of_disconnected_cells;
-            bestSimLessDisconnectedCells = sim;
-            better = true;
-        }
-        qDebug() << "Sim" << g << ", TX" << sim->baseStations[0]->getCoordinates() << ", average power:" << average_pow_dbm << "dBm";
-        if (!better) {
-            sim_list.removeAt(0);
-        }
-    }
-*/
-    //qDebug() << "Best sim average power: transmitter at" << bestSimAveragePower.baseStations[0]->getCoordinates();
     qDebug() << "Best sim average power: transmitter at" << bestSimAveragePowerTX;
     qDebug() << "Best average power:" << best_average_pow_dbm << "dBm";
     qDebug() << "-----------";
-    //qDebug() << "Best sim min num of disconnected cells: transmitter at" << bestSimLessDisconnectedCells.baseStations[0]->getCoordinates();
     qDebug() << "Best sim min num of disconnected cells: transmitter at" << bestSimLessDisconnectedCellsTX;
     qDebug() << "Min num of disconnected cells:" << min_num_of_disconnected_cells;
 
     //qDebug() << "best sim p*:" << &bestSimAveragePower;
-    //return &bestSimAveragePower;
 }
